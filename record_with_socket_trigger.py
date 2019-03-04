44c099e725697ae08b5d3bbf59ccb9ca98faf2f5
PWINZELL <peter.winzell@volvocars.com>
2019-03-04 23:25:52 +0000

    preps for firebase

--- record_with_socket_trigger.py
+++ record_with_socket_trigger.py
@@ -4,14 +4,12 @@ import picamera
 import time
 import picamera.array
 import numpy as np
-import socket
 import subprocess
 import fnmatch
 import os
 import json
 import _thread
 
-from getip import get_ip_address # bind to server address
 from PIL import Image, ImageDraw
 
 prior_image = None
@@ -60,28 +58,8 @@ def removeOldestFile():
             print (mp4file)                           
 
 #new connections handled as separate threads
-def on_new_client(clientsocket,addr):
-    while True:
-        jsoncommandStr = clientsocket.recv(1024)
-        if not jsoncommandStr:
-            break;
-        #try:
-        commandJSON = json.loads(jsoncommandStr.decode())
-        commandstring = commandJSON["command"]
-        print(commandstring)
-        if   (commandstring == 'motion'):
-            print("is here")
-            camera_record()
-        elif (commandstring == 'incident'):
-            tag_file()
-        else:
-            continue    
-        #except Exception as e:
-         #   print(e)
-        print ("command:   " + commandJSON["command"])
-        print ("cameraId:  " + repr(commandJSON["cameraId"]))
-        print ("timestamp: " + repr(commandJSON["timestamp"]))
-    clientsocket.close()
+def on_new_client():
+        camera_record()
 
 #tag recorded file with incident stamp
 def tag_file():
@@ -119,53 +97,7 @@ def camera_record():
                 print('failed to convert to mp4') 
      
 def main():
-    
-    # setup socket server
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    host = "10.242.179.156" #get_ip_address()
-    port = 49152
-    
-    s.bind((host,port))
-    s.listen(5)
-    
-    while True:
-        # wait for a connection which we use as trigger to start a 10s recording
-        print('waiting for connection...')
-        c,addr = s.accept() 
-        #wait for command from client
+    _thread.start_new_thread(on_new_client,())
         
-        _thread.start_new_thread(on_new_client,(c,addr))
-        
-        # setup and start camera recording
-        # removeOldestFile()                       
-        #stream = picamera.PiCameraCircularIO(camera, seconds=10)
-       # motion_detector = MotionDetector(camera)
-       # camera.start_recording(
-        #        stream, format='h264', bitrate=REC_BITRATE,
-         #       intra_period=REC_FRAMERATE)
-
-        #print('recording starting...')
-        #try:
-          #  while True:
-                ##camera.split_recording('after.h264')
-           #     time.sleep(10);
-            #    camera.split_recording('after.h264')
-             #   write_before(stream)
-              #  break
-       # finally:
-        #    print('recording stops...')
-         #   camera.stop_recording()
-          #  reportfilename = "videos/" + getTimeStampedFileName()
-           # command = "MP4Box -add {} {}.mp4".format('before.h264',reportfilename)
-            #try:
-             #   output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
-           # except subprocess.CalledProcessError as e:    
-             #   print('failed to convert to mp4')    
-
-
 if __name__ == '__main__':
-    #with picamera.PiCamera() as camera:
-        #camera.resolution = (1280, 720)
-        #main(camera)
     main()
