24b9250b31611befd58d9305b3a0cf36aa25df4d
PWINZELL <peter.winzell@volvocars.com>
2019-03-13 15:25:19 -0700

    exception of exception fix

--- connection.py
+++ connection.py
@@ -6,15 +6,15 @@ import urllib3
 # currently against google com
 URL = "http://www.google.com"
 
-def has_internetconnection():
+def has_internetconnection(logger):
     while True:
         http = urllib3.PoolManager()
         try:
             http.request('GET',URL,retries=False)
         except urllib3.exceptions.NewConnectionError as e:
-            print(e)
-            print("not connected to the internet")
+            logger.debug(e)
+            logger.debug("not connected to the internet")
             return False
         else:
-            print("connected to the internet")
+            logger.debug("connected to the internet")
             return True
