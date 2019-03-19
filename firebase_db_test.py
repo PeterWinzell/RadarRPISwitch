f19ea706f4a39be4c3b30ed696201edb7ca1aef1
PeterWinzell <peterwinzell.gbg@gmail.com>
2019-03-19 11:13:55 -0700

    class for db conncetion in firebase_db_test.py

--- firebase_db_test.py
+++ firebase_db_test.py
@@ -3,22 +3,24 @@ from firebase_admin import credentials, firestore, storage, db, auth
 
 # get current directory
 MAIN_DIR, _ = os.path.split(os.path.abspath(__file__))
-
+PARTITION_NAME = "pics/" # should be added through
+CREDFILENAME = ""
 
 class FirebaseDbUtility:
+    """Handles the upload of video files to firebase"""
     cred = None
     app = None
 
-    def init(self, credfilename):
+    def __init__(self, credfilename):
         self.cred = credentials.Certificate(MAIN_DIR + credfilename)
         self.app = firebase_admin.initialize_app(self.cred, {
             'storageBucket': 'damagereport-897b3.appspot.com',
             'databaseURL': 'https://damagereport-897b3.firebaseio.com',
             'databaseAuthVariableOverride': {
-                'uid': 'qLlrevFUUPhKuVwx7O3hUFRsdUx2'}
+                'uid': 'qLlrevFUUPhKuVwx7O3hUFRsdUx2'} 
         })
 
-    def writeUrlToDB(urlstr):
+    def writeurltoB(self, urlstr):
         root = db.reference()
         root.child('damagereports').push({
             'url': urlstr
@@ -28,10 +30,10 @@ class FirebaseDbUtility:
         db = firestore.client()
         bucket = storage.bucket()
         line = filename
-        _, _, blobname = line.partition("pics/")
+        _, _, blobname = line.partition(PARTITION_NAME)
 
         blob = bucket.blob("damagereport/" + blobname)
-        print("Blob oath is : " + blob.path)
+        print("Blob path is : " + blob.path)
         filepath = filename
 
         # blob.make_public()
@@ -104,15 +106,12 @@ def writeURLtoDB(urlstr):
 def downLoadBlob():
     print(" downloading... ")
 
-def testingDbClass():
-    databaseutility = iniateDbConnection("/damagereport.json")
-    databaseutility.init()
-    databaseutility.uploadBlob()
 
 def main():
-    iniateDbConnection2()
-    # writeURLtoDB()
-    uploadBlobFile(MAIN_DIR + '/pics/pic0.jpg')
+    # initialize database
+    dbutil = FirebaseDbUtility("/damagereport.json")
+    # upload file to firebase
+    dbutil.uploadBlob('pics/pic0.jpg')
 
 
 if __name__ == '__main__':
